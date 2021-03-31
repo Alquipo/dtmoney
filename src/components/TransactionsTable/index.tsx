6a147fa98c76609d955c91bd68e692c637bb3173
Alquipo <alquiponeto@outlook.com.br>
2021-03-31 19:00:28 -0300

    fix: seeds api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+type TransactionData = {
+  id: number
+  title: string
+  amount: number
+  type: string
+  category: string
+  createdAt: string
+}
+
 export function TransactionsTable(){
+  const [transactions, setTransactions] = useState<TransactionData[]>([])
 
   useEffect(() => {
-    api.get('transactions').then(response => console.log(response.data))
+    api.get('transactions').then(response => setTransactions(response.data.transactions))
   }, [])
 
   return (
@@ -21,20 +31,14 @@ export function TransactionsTable(){
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>31/03/2021</td>
-          </tr>
-
-          <tr>
-            <td>Alugue</td>
-            <td className="withdraw">-R$1.100</td>
-            <td>Casa</td>
-            <td>30/03/2021</td>
-          </tr>
-
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
 
         </tbody>
       </table>
